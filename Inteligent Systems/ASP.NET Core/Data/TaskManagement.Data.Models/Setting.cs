5a95f3073f559e06d6d9b994b3d12cfe0d8efe72
Tenyo <THEG4ME@abv.bg>
2020-05-13 18:11:32 +0300

    ASP TaskManagement project.

---
+++ Inteligent Systems/ASP.NET Core/Data/TaskManagement.Data.Models/Setting.cs
@@ -0,0 +1,11 @@
+﻿namespace TaskManagement.Data.Models
+{
+    using TaskManagement.Data.Common.Models;
+
+    public class Setting : BaseDeletableModel<int>
+    {
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
---
+++ Inteligent Systems/ASP.NET Core/Web/TaskManagement.Web/Areas/Administration/Controllers/AdministrationController.cs
@@ -0,0 +1,14 @@
+﻿namespace TaskManagement.Web.Areas.Administration.Controllers
+{
+    using TaskManagement.Common;
+    using TaskManagement.Web.Controllers;
+
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Authorize(Roles = GlobalConstants.AdministratorRoleName)]
+    [Area("Administration")]
+    public class AdministrationController : BaseController
+    {
+    }
+}
